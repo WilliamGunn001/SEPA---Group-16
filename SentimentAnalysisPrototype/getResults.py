c13c50f71e2a6408d34230ac87b3aea9f73ddf09
daisyaczelmorris <101616851@student.swin.edu.au>
2022-05-16 12:46:24 +1000

    UI
    updated project so the UI Shows the graphs when the generate button is hit

---
+++ SentimentAnalysisPrototype/getResults.py
@@ -0,0 +1,30 @@
+import argparse
+import sys
+import pandas as pd
+from MyWidget import MyWidget
+from PySide6.QtCore import QDateTime, QTimeZone
+from PySide6.QtWidgets import QApplication 
+class getResults(object):
+    """description of class"""
+
+   
+    def read_data(self,fname):
+        # Read the CSV content
+        df = pd.read_csv(fname)
+        # Remove wrong magnitudes
+
+        dates = df["Date"].apply(lambda x: self.date(x))
+        sentiment= df["Sentiment_Score"]
+
+        # My local timezone
+
+        # Get timestamp transformed to our timezone
+
+        return dates, sentiment
+  
+    def date(self,utc):
+            utc_fmt = "MM-dd-yyyy"
+            timezone = QTimeZone(b"Australia/Sydney")
+
+            new_date = QDateTime().fromString(utc, utc_fmt)
+            return new_date    
---
+++ SentimentAnalysisPrototype/main.py
@@ -0,0 +1,19 @@
+from UI import Main
+from PySide6 import QtWidgets
+from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog, QLineEdit, QLabel, QPushButton, QBoxLayout, QVBoxLayout, QHBoxLayout, QWidget, QComboBox, QRadioButton, QTextEdit, QSpinBox
+import sys
+import os
+from datetime import datetime, timedelta
+from PySide6.QtCore import Qt
+
+def main():
+    app = QApplication(sys.argv)
+    win = Main()
+    while True:
+        win.show()
+        win.update()
+        sys.exit(app.exec())
+    
+
+if __name__ == "__main__":
+    main()
